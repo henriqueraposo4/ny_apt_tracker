aaee585c3d1f6d93cb5845d8fc41b43c1910dfd5
Henrique Raposo <137647812+henriqueraposo4@users.noreply.github.com>
2025-04-11 16:53:56 -0400

    Update ApartmentDetails.java

--- ApartmentDetails.java
+++ ApartmentDetails.java
@@ -3,25 +3,36 @@
 public class ApartmentDetails {
     private String price;
     private String location;
-    private String distance;
+    private String neighborhood;
+    private int bathrooms;
     private int bedrooms;
+    private String distanceToWorkY;
     private String amenities;
+    private String originalUrl;
 
     // Default constructor with placeholder data
     public ApartmentDetails() {
         this.price = "N/A";
         this.location = "N/A";
-        this.distance = "N/A";
+        this.neighborhood = "N/A";
+        this.bathrooms = 0;
         this.bedrooms = 0;
+        this.distanceToWorkY = "N/A";
         this.amenities = "N/A";
+        this.originalUrl = "N/A";
     }
 
     // Getters and Setters would be added here as needed
 
     @Override
     public String toString() {
-        return "Price: " + price + ", Location: " + location +
-               ", Distance from work: " + distance + ", Bedrooms: " + bedrooms +
-               ", Amenities: " + amenities;
+        return "Price: " + price +
+               ", Location: " + location +
+               ", Neighborhood: " + neighborhood +
+               ", Bathrooms: " + bathrooms +
+               ", Bedrooms: " + bedrooms +
+               ", Distance to Work Y: " + distanceToWorkY +
+               ", Amenities: " + amenities +
+               ", Original URL: " + originalUrl;
     }
 }
